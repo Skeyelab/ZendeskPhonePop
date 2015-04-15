12431012a6ba8f7541ebe24c90f2ad3a419069e2
Eric Dahl <edahl@groupon.com>
2015-04-15 16:25:41 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,34 @@
+(function() {
+
+  var TICKET_POP_URI = '/api/v2/channels/voice/tickets.json';
+
+  return {
+    requests: {
+
+      popTicket: function(data) {
+        return {
+          url:         TICKET_POP_URI,
+          type:        'POST',
+          data:        data
+        };
+      }
+    },
+    events: {
+      'app.activated':'intialize',
+      'click .btn':'createTicketAndPop',
+      'popTicket.done': function() {
+		this.popover('hide');
+       }
+    },
+
+    intialize: function() {
+        this.switchTo('button');
+    },
+    createTicketAndPop: function() {
+		var data = {"display_to_agent":this.currentUser().id(),"ticket":{"subject":"Phone Ticket","comment":{"body":"Phone Ticket created by Phone Pop App","public":false},"priority":"urgent","via_id":45,"assignee_id":this.currentUser().id()}};
+		this.ajax('popTicket', data);
+    }
+  };
+
+}());
+
