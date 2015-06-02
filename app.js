dd7bf00ff03e0b6929f8a5948da4ca32cd9e57a4
Eric Dahl <edahl@groupon.com>
2015-06-02 13:42:39 -0500

    2015-06-02 1:42

--- app.js
+++ app.js
@@ -7,28 +7,70 @@
 
       popTicket: function(data) {
         return {
-          url:         TICKET_POP_URI,
-          type:        'POST',
-          data:        data
+          url: TICKET_POP_URI,
+          type: 'POST',
+          dataType: 'json',
+          data: JSON.stringify(data),
+          contentType: 'application/json',
+          proxy_v2: true
         };
       }
     },
     events: {
-      'app.activated':'intialize',
-      'pane.activated':'createTicketAndPop',
+      'app.activated': 'intialize',
+      'click .btn': 'displayPanel',
+      'click .button-confirm': 'createTicketAndPop',
       'popTicket.done': function() {
-		this.popover('hide');
-       }
+        this.popover('hide');
+      }
     },
 
     intialize: function() {
-        this.switchTo('button');
+      this.switchTo('button');
+
     },
+
+    displayPanel: function() {
+      this.switchTo('panel');
+    },
+
+
     createTicketAndPop: function() {
-		var data = {"display_to_agent":this.currentUser().id(),"ticket":{"subject":"Phone Ticket","comment":{"body":"Phone Ticket created by Phone Pop App","public":false},"priority":"urgent","via_id":45,"assignee_id":this.currentUser().id()}};
-		this.ajax('popTicket', data);
+      this.security_code = this.$('.security_code').val();
+      this.country = this.$('.country').val();
+      this.customer_language = this.$('.customer_language').val();
+      this.email_address = this.$('.email_address').val();
+      var data = {
+        "display_to_agent": this.currentUser().id(),
+        "ticket": {
+          "subject": "Phone Ticket",
+          "requester": {
+            "email": this.email_address
+          },
+          "tags": ["phone"],
+          "custom_fields": [{
+            "id": this.setting('security_code'),
+            "value": this.security_code
+          }, {
+            "id": this.setting('country'),
+            "value": this.country
+          }, {
+            "id": this.setting('customer_language'),
+            "value": this.customer_language
+          }],
+          "comment": {
+            "body": "Phone Ticket created by Phone Pop App",
+            "public": false
+          },
+          "priority": "urgent",
+          "via_id": 45,
+          "assignee_id": this.currentUser().id()
+        }
+      };
+
+      this.ajax('popTicket', data);
+
     }
   };
 
 }());
-
